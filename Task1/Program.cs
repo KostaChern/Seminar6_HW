7d705b1f5f8201910615c6f9ba40e296ef3f6f12
Konstantin Chern <codahl@yandex.ru>
2023-03-02 15:22:44 +0300

    Solved Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+
+int InputInt(string msg)
+{
+    Console.Write($"{msg} -> ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] Array(int size)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(-25, 25);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int item in array)
+    {
+        System.Console.Write($"{item} ");
+    }
+    System.Console.WriteLine();
+}
+
+
+
+int negativElement(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < 0)   // определяем отрицательные элементы
+        {
+            sum++;
+        }
+    }
+    Console.WriteLine($"количество отрицательно введеных чисел = {sum}");
+    return sum;
+}
+
+int len = InputInt("Введите длину массива");
+int[] array = Array(len);
+PrintArray(array);
+int sum = negativElement(array);
+
+
+
+
+
+
+
